f186c4e4c4b93a40b971f7aba238c660a456ddbc
HammyPig <jamesbwalden@protonmail.com>
2022-09-03 15:28:09 +1000

    feat: keyboard interrupt operations

--- src/curator.py
+++ src/curator.py
@@ -47,26 +47,31 @@ class Curator:
         return unique_path
 
     def curate_from_source(self, path):
+        operation_cancelled_str = "Operation cancelled.\n"
+
         # collect image files
-        file_paths = []
-        for path, subdirs, files in os.walk(path):
-            for name in files:
-                file_path = os.path.join(path, name)
-                file_extension = file_path.split(".")[-1]
-                if file_extension in ("jpg", "png"):
-                    file_paths.append(file_path)
-        
+        try:
+            file_paths = []
+            for path, subdirs, files in os.walk(path):
+                for name in files:
+                    file_path = os.path.join(path, name)
+                    file_extension = file_path.split(".")[-1]
+                    if file_extension in ("jpg", "png"):
+                        file_paths.append(file_path)
+        except KeyboardInterrupt:
+            print(operation_cancelled_str)
+            return
+
         continue_prompt = input(f"{len(file_paths)} image files ready to be curated. Do you wish to continue? y/n ")
         print()
 
         if continue_prompt != "y":
-            print("Operation cancelled.\n")
+            print(operation_cancelled_str)
             return
 
-        # create copy queue
-        copy_queue = []
-        for file_path in file_paths:
-            copy_queue.append((file_path, self.curated(file_path)))
-
-        for old_file_path, new_file_path in copy_queue:
-            shutil.copy2(old_file_path, Curator.unique(new_file_path))
+        try:
+            for file_path in file_paths:
+                shutil.copy2(file_path, self.unique(self.curated(file_path)))
+        except KeyboardInterrupt:
+            print(operation_cancelled_str)
+            return
