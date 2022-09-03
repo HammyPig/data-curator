53541fac5c919f1fc474c8bfafaf827f68726f3b
HammyPig <jamesbwalden@protonmail.com>
2022-09-03 15:40:19 +1000

    docs: curator functions

--- src/curator.py
+++ src/curator.py
@@ -15,6 +15,17 @@ class Curator:
         self.archive_path = archive_path
 
     def curated(self, file_path):
+        """
+        Return a curated file path.
+
+            Parameters
+                file_path (str): A file path e.g. C:/Users/James/Pictures/hello.jpg
+            
+            Returns:
+                curated_file_path (str): A file path in the form of:
+                    <archive_path><time_created>_<original_filename>
+                    e.g. D:/archive/images/photos/2022-09-03_hello.jpg
+        """
         date_modified = os.path.getmtime(file_path)
         date_modified = datetime.datetime.fromtimestamp(date_modified)
         date_modified_str = date_modified.strftime("%Y-%m-%d")
@@ -32,21 +43,17 @@ class Curator:
 
         return f"{self.archive_path}{time_created_str}_{filename}"
 
-    def unique(file_path):
-        unique_path = file_path
-        ext_i = file_path.rfind(".")
-
-        n = 1
-        while True:
-            if os.path.exists(unique_path):
-                unique_path = f"{file_path[:ext_i]}_COPY{n}{file_path[ext_i:]}"
-                n += 1
-            else:
-                break
-        
-        return unique_path
-
     def curate_from_source(self, path):
+        """
+        Copies all images from a source directory and pastes them into the
+        curated file path. Handles duplicate file paths automatically.
+
+            Parameters
+                path (str): A file path e.g. C:/Users/James/Pictures/
+            
+            Returns:
+                None
+        """
         operation_cancelled_str = "Operation cancelled.\n"
 
         # collect image files
@@ -75,3 +82,29 @@ class Curator:
         except KeyboardInterrupt:
             print(operation_cancelled_str)
             return
+
+    @staticmethod
+    def unique(file_path):
+        """
+        Return a unique file path by adding _COPY<n> if the file path already exists.
+
+            Parameters
+                file_path (str): A file path e.g. C:/Users/James/Pictures/hello.jpg
+            
+            Returns:
+                unique_path (str): A file path that does not exist
+                    e.g. C:/Users/James/Pictures/hello.jpg
+                    e.g. C:/Users/James/Pictures/hello_COPY.jpg
+        """
+        unique_path = file_path
+        ext_i = file_path.rfind(".")
+
+        n = 1
+        while True:
+            if os.path.exists(unique_path):
+                unique_path = f"{file_path[:ext_i]}_COPY{n}{file_path[ext_i:]}"
+                n += 1
+            else:
+                break
+        
+        return unique_path
